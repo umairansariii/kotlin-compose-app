d60cea4997963b187f6e2705a03397387baca5e2
umairansariii <umairansari.work@gmail.com>
2025-01-27 03:44:02 +0500

    feat: add bottom navigation

---
+++ app/src/main/java/com/example/composeapp/components/BottomNavigationBar.kt
@@ -0,0 +1,92 @@
+package com.example.composeapp.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.composeapp.data.BottomNavigationItem
+
+val items = listOf(
+    BottomNavigationItem(
+        title = "Home",
+        route = "home",
+        selectedIcon = Icons.Filled.Home,
+        unselectedIcon = Icons.Outlined.Home,
+        hasNews = false,
+        badgeCount = 0,
+    ),
+    BottomNavigationItem(
+        title = "Notifications",
+        route = "notifications",
+        selectedIcon = Icons.Filled.Notifications,
+        unselectedIcon = Icons.Outlined.Notifications,
+        hasNews = false,
+        badgeCount = 15,
+    ),
+    BottomNavigationItem(
+        title = "Settings",
+        route = "settings",
+        selectedIcon = Icons.Filled.Settings,
+        unselectedIcon = Icons.Outlined.Settings,
+        hasNews = true,
+        badgeCount = 0
+    ),
+)
+
+@Composable
+fun BottomNavigationBar() {
+    NavigationBar {
+        Row(
+            modifier = Modifier.background(MaterialTheme.colorScheme.inverseOnSurface)
+        ) {
+            items.forEachIndexed { index, item ->
+                NavigationBarItem(
+                    selected = index == 0,
+                    onClick = {},
+                    icon = {
+                        BadgedBox(
+                            badge = {
+                                if (item.badgeCount != 0) {
+                                    Badge {
+                                        Text(text = item.badgeCount.toString())
+                                    }
+                                } else if (item.hasNews) {
+                                    Badge()
+                                }
+                            }
+                        ) {
+                            Icon(
+                                imageVector =
+                                if (index == 0) {
+                                    item.selectedIcon
+                                } else {
+                                    item.unselectedIcon
+                                },
+                                contentDescription = item.title,
+                                tint = MaterialTheme.colorScheme.onBackground,
+                            )
+                        }
+                    },
+                    label = {
+                        Text(text = item.title)
+                    }
+                )
+            }
+        }
+    }
+}
+
