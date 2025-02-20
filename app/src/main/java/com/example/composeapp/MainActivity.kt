096abc552b4faae179c84bc9d7025f2a0d55d6e5
umairansariii <umairansari.work@gmail.com>
2025-02-20 05:20:35 +0500

    feat: setup navigation routes

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
-import com.example.composeapp.components.BottomNavigationBar
-import com.example.composeapp.auth.presentation.login.LoginScreen
-import com.example.composeapp.auth.presentation.register.RegisterScreen
+import com.example.composeapp.routes.AppNavigation
 import com.example.composeapp.ui.theme.ComposeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,16 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeAppTheme {
-                Scaffold(
-                    bottomBar = {
-                        BottomNavigationBar()
-                    },
-                    modifier = Modifier.fillMaxSize()
-                ) { innerPadding ->
-                    RegisterScreen(
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppNavigation()
             }
         }
     }
