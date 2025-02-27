f0824a116aaa0edfd085e3ecaa9e8be7523f281c
umairansariii <umairansari.work@gmail.com>
2025-02-27 15:41:58 +0500

    feat: implement posts fetching

--- app/src/main/java/com/example/composeapp/app/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/composeapp/app/presentation/home/HomeScreen.kt
@@ -6,28 +6,40 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.composeapp.model.Quote
+import com.example.composeapp.data.model.Post
 
 @Composable
 fun HomeScreen() {
-    val viewModel = viewModel<HomeViewModel>()
-    var quotes by remember { mutableStateOf(emptyList<Quote>()) }
+    val viewModel = HomeViewModel()
+    var posts by rememberSaveable { mutableStateOf(emptyList<Post>()) }
 
-//    LazyColumn(modifier = Modifier.fillMaxSize()) {
-//        items(list) {
-//            Column(modifier = Modifier.fillMaxWidth()) {
-//                Text(text = it.id.toString())
-//                Spacer(modifier = Modifier.height(8.dp))
-//                Text(text = it.quote)
-//            }
-//        }
-//    }
+    LaunchedEffect(Unit) {
+        posts = viewModel.getPosts()
+    }
+
+    Surface(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            items(posts) { post ->
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    Text(text = post.id.toString())
+                    Spacer(Modifier.height(8.dp))
+                    Text(text = post.title, style = MaterialTheme.typography.bodyLarge)
+                    Spacer(Modifier.height(8.dp))
+                    Text(text = post.body, style = MaterialTheme.typography.bodySmall)
+                }
+            }
+        }
+    }
 }
